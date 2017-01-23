c6636e61579b2be44a51517f5b2c706249aa1bdf
pranalipatil209 <pranalipatil209@gmail.com>
2017-01-23 09:44:22 +0530

    server at heroku

--- server/app/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * SecretKey and Database
- * @module config
- * */
-module.exports = {
-	'secret' : 'ilovechoco-lavacake',
-	'database' : 'mongodb://pranali:heroku123@ds129028.mlab.com:29028/heroku_fpr5t90p'
-};
