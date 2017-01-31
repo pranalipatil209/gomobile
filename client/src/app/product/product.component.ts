bd5b6e961e6eef60fa7e91390cbde01528734d71
pranalipatil209 <pranalipatil209@gmail.com>
2017-01-31 15:41:39 +0530

    .bowerrc added

--- client/src/app/product/product.component.ts
+++ client/src/app/product/product.component.ts
@@ -13,23 +13,7 @@ export class MyProduct implements OnInit {
 
   constructor() { }
   @Input() prod: product;
-// = {
-//   "id": 1,
-//   "name": "Sony Xperia Z3",
-//   "price": 899,
-//   "specs": {
-//     "manufacturer": "Sony",
-//     "storage": 16,
-//     "os": "Android",
-//     "camera": 15
-//   },
-//   "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique ipsum in efficitur pharetra. Maecenas luctus ante in neque maximus, sed viverra sem posuere. Vestibulum lectus nisi, laoreet vel suscipit nec, feugiat at odio. Etiam eget tellus arcu.",
-//   "rating": 4,
-//   "image": {
-//     "small": ".././assets/iphone6.jpg",
-//     "large": ".././assets/iphone6-large.jpg"
-//   }
-// };
+
   ngOnInit() {
 
   }
