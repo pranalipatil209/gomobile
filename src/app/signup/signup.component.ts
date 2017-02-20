464726dcbadcb7fd03c3339aec00117d2546ff41
pranalipatil209 <pranalipatil209@gmail.com>
2017-02-20 16:37:55 +0530

    add

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,49 +1,43 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RouterModule, Routes, Router } from '@angular/router';
-import { Http, Response, Request, RequestMethod } from '@angular/http';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {RouterModule, Routes, Router} from '@angular/router';
+import {Http, Response, Request, RequestMethod} from '@angular/http';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+    selector: 'app-signup',
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
 })
-export class SignupComponent{
+export class SignupComponent {
 
-	signUpForm : FormGroup;
-	private load = false;
+    signUpForm: FormGroup;
+    private load = false;
 
-	constructor(fb: FormBuilder, public http: Http ,private router: Router) {
+    constructor(fb: FormBuilder, public http: Http, private router: Router) {
 
-		this.signUpForm = fb.group({
-			'name' : [null, Validators.required],
-			'email' : [null,Validators.required],
-			'mobile' : [null, Validators.compose([Validators.required, Validators.minLength(10)])],
-			'password' : [null, Validators.compose([Validators.required, Validators.minLength(8)])]
-		})
-	}
+        this.signUpForm = fb.group({
+            'name': [null, Validators.required],
+            'email': [null, Validators.required],
+            'mobile': [null, Validators.compose([Validators.required, Validators.minLength(10)])],
+            'password': [null, Validators.compose([Validators.required, Validators.minLength(8)])]
+        })
+    }
 
-	submitForm(value:any):void{
-	  this.load = true;
+    submitForm(value: any): void {
+        this.load = true;
+        this.http.post('https://choco-lava.herokuapp.com/api/signup', value).subscribe(
+            (res: any) => {
+                this.load = false;
+                let data = res.json();
+                if (data.data == "User already exist!") {
+                    alert(data.data);
+                }
+                else {
+                    this.router.navigate(['/login']);
+                }
+            })
+    }
 
-		this.http.post('https://choco-lava.herokuapp.com/api/signup',value).subscribe(
-		(res:any)=>{
-		  this.load = false;
-<<<<<<< HEAD
-			this.router.navigate(['/login']);
-=======
-			let data = res.json();
-			console.log(data);
-			if(data.data == "User already exist!"){
-				alert(data.data);
-			}
-			else{
-				this.router.navigate(['/login']);
-			}
->>>>>>> 24043714ca7bb52d8978c0d8fea998db910e92a0
-		})
-	}
-
-  }
+}
 
 
